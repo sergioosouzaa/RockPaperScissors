4f7e354f453286bb52a464231f2f60c9530c0ad2
Sergio Souza <sergiosouzauff@gmail.com>
2022-02-16 13:17:55 -0300

    created layout

--- js/game.js
+++ js/game.js
@@ -23,21 +23,24 @@ function checkValidSelection (playerSelection) {
     return checkResult;
 }
 
+//create a selection of the div, to show round result
+const roundResult = document.querySelector('.roundResult');
+
 //check if is a tie, win or lose. print the result with console.log, return a number correponding with the result
 function compareChoices (playerSelection, computerSelection) {
     let compareResult;
     let result;
     if (playerSelection === computerSelection) {
-        compareResult = `It's a tie! You've both selected ${playerSelection}!`
+        compareResult = `Round Result: It's a tie! You've both selected ${playerSelection}!`
         result = 0;
     } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
-        compareResult = `You won! ${playerSelection} beats ${computerSelection}!`
+        compareResult = `Round Result: You won! ${playerSelection} beats ${computerSelection}!`
         result = 1;
     } else {
-        compareResult = `You lost! ${computerSelection} beats ${playerSelection}!`
-        result = 2;
+        compareResult = `Round Result: You lost! ${computerSelection} beats ${playerSelection}!`
+        result = -1;
     }
-    console.log(compareResult);
+    roundResult.textContent = compareResult;
     return result;
 }
 
@@ -54,30 +57,63 @@ function playRound(playerSelection, computerSelection) {
     return roundResult;
 }
 
-// run 5 rounds of the game, record the result. If the user input is invalid run the game again
-function game () {
-    let playerSelection;
-    let wins = 0;
-    let tie = 0;
-    let looses = 0;
-    let computerSelection;
-    let matchResult;
+const gameResult = document.querySelector('.gameResult');
+let wins = 0;
+let tie = 0;
+let looses = 0;
+let points = 0;
 
-    for(let i = 0; i < 5; i++) {
-        playerSelection = window.prompt();
-        computerSelection = computerPlay();
-        matchResult = playRound(playerSelection, computerSelection);
-        if (matchResult == 0){
-            tie++;
-        } else if (matchResult == 1) {
-            wins++;
-        } else if (matchResult == -1) {
-            i--;
-        }else {
-            looses++;
-        }
+// run 5 rounds of the game, record the result. If the user input is invalid run the game again
+function game (matchResult) {
+
+  
+    if (matchResult == 0){
+        tie++;
+    } else if (matchResult == 1) {
+        wins++;
+        points++;
+    } else if (matchResult == -1) {
+        looses++;
     }
-    console.log(`${wins} wins. ${tie} ties. ${looses} loses.`)
+
+    if(points === 5)
+    {
+        gameResult.textContent = `Match results: ${wins} wins. ${tie} ties. ${looses} loses.`;
+        roundResult.textContent ='';
+    } else {
+        gameResult.textContent = `${wins} wins. ${tie} ties. ${looses} loses.  ${points} points.`;
+    }
 }
 
-game();
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', function () {
+    game(playRound("rock", computerPlay()));
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', function () {
+    game(playRound("paper", computerPlay()));
+});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', function () {
+    game(playRound("scissors", computerPlay()));
+});
+
+
+// const rock = document.querySelector('#rock');
+// rock.addEventListener('click', function () {
+//     playRound("rock", computerPlay());
+// });
+
+// const paper = document.querySelector('#paper');
+// paper.addEventListener('click', function () {
+//     playRound("paper", computerPlay());
+// });
+
+// const scissors = document.querySelector('#scissors');
+// scissors.addEventListener('click', function () {
+//     playRound("scissors", computerPlay());
+// });
+
+
